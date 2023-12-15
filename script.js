81a1411fcde552f3444054cff22cea55d7484b36
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-12-15 16:24:43 +0530

    Add the JS code for add,subtract,multiply,divide

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let num1;
+num1 = prompt("Enter first number");
+num1 = Number(num1);
+
+let operator;
+operator = prompt("Enter operator");
+
+let num2;
+num2 = prompt("Enter second number");
+num2 = Number(num2);
+
+function operate(num1,operator,num2){
+    switch(operator){
+        case '+':
+            console.log(add(num1,num2));
+            break;
+        case '-':
+            console.log(subtract(num1,num2));
+            break;
+        case '*':
+            console.log(multiply(num1,num2));
+            break;
+        case '/':
+            console.log(divide(num1,num2));
+            break;
+    }
+}
+
 function add(number1,number2){
     return number1+number2;
 }
@@ -9,8 +37,10 @@ function multiply(number1,number2){
 }
 function divide(number1,number2){
     if(number2 === 0){
-        alert("Divide by zero Error!");
+        console.log("Divide by zero Error!");
         return;
     }
     return number1/number2;
 }
+
+operate(num1,operator,num2);
